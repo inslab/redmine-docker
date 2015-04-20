e98192ac35e0315310335fb60c5a7b68497342b4
inslab.dev <inslab.dev@gmail.com>
2015-04-20 17:55:41 +0900

    use init script when create database instead of createdb.sh

--- createdb.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo '=============================='
-echo 'create database '${DB_NAME} > /app/setup/createdb.sql
-cat /app/setup/createdb.sql
-
-mysql -h${DB_HOST} -P${DB_PORT} -u${DB_USER} -p${DB_PASS} < /app/setup/createdb.sql
-
